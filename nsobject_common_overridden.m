c428f66673d78a1b0d699944fd52edf062275712
Cong Hui <lestathc@lestathc-macpro.roam.corp.google.com>
2014-10-31 15:42:35 -0700

    Imported user code snippets

---
+++ nsobject_common_overridden.m
@@ -0,0 +1,25 @@
+// NSObject Common Overridden
+// The common overriden methods for NSObject
+//
+// IDECodeSnippetCompletionPrefix: prgma_mark_nsobject
+// IDECodeSnippetCompletionScopes: [ClassInterfaceMethods]
+// IDECodeSnippetIdentifier: BD7EDBB3-D6D4-43AE-89C2-64D88605B8AD
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+#pragma mark - NSObject
+
+/**
+ *  Override
+ */
+- (NSUInteger)hash;
+
+/**
+ *  Override
+ */
+- (BOOL)isEqual:(id)object;
+
+/**
+ *  Override
+ */
+- (NSString *)description;
